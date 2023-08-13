ac1247503255cb8c821ed2f10afc9ef5b4d4c3d3
galinapivtorak <132648535+galinapivtorak@users.noreply.github.com>
2023-08-13 11:21:48 +0300

    Add files via upload

---
+++ pivtorak_tests_python_insert_update_delete/tests/test_insert_cl_1.py
@@ -0,0 +1,22 @@
+import sqlite3
+import pytest
+
+
+def test_insert_row_table_client():  # дали имя функции создания таблицы
+    conn1 = sqlite3.connect(':memory:')  # подключение к базе данных в памяти
+    c = conn1.cursor()  # создание курсора для выполнения запросов
+    c.execute('''CREATE TABLE client
+                 (id INTEGER PRIMARY KEY NOT NULL,
+                 name TEXT NOT NULL,
+                 change_date TEXT NOT NULL)''')  # курсор создал таблицу из трех полей
+
+    c.execute(
+        "INSERT INTO client (id, name, change_date) VALUES (1, 'Masha', '2001-01-01')")  # вставляем данные в таблицу
+    conn1.commit()  # подтвердили создание таблицы в подключении
+
+    c.execute("SELECT * FROM client WHERE id=1")  # выбираем имена из таблицы, где id=1
+    result = c.fetchone()  # отобранные строки записываем в объект (переменную) result
+    conn1.close() # конец подключения к базе данных в памяти
+    return result # возврат результата селекта
+
+print(test_insert_row_table_client()) # выводим в терминал результат
